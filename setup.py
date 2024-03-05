b0e1c6d8dfe07a4ebd7357b9458b85003647d85c
faradox <felix@okio.ai>
2024-03-05 09:28:58 +0100

    chore: prepare release 0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 if __name__ == "__main__":
     setup(
         name="nendo-plugin-caption-lpmusiccaps",
-        version="0.1.1",
+        version="0.1.2",
         description="An audio captioning plugin based on LP-MusicCaps.",
         author="Aaron Abebe <aaron@okio.ai>",
     )
